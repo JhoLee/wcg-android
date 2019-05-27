6fc84ba81e5386104376a57e277b7e2687f328e8
Pak SangWon <pphilio@naver.com>
2019-05-27 16:47:32 +0900

    add intro

--- app/src/main/java/me/jho/wcg/MainActivity.java
+++ app/src/main/java/me/jho/wcg/MainActivity.java
@@ -1,5 +1,6 @@
 package me.jho.wcg;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -38,6 +39,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        boolean isRunIntro = getIntent().getBooleanExtra("intro", true);
+        if (isRunIntro) {
+            beforeIntro();
+        } else {
+            //afterIntro(savedInstanceState);
+        }
         // [START Fragments]
         fragmentManager = getSupportFragmentManager();
         wordCloudFragment = new WordCloudFragment();
@@ -77,5 +84,27 @@ public class MainActivity extends AppCompatActivity {
         // [END bottomNavigationView]
     }
 
+    private void beforeIntro() {
+        getWindow().getDecorView().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.putExtra("intro", false);
+                startActivity(intent);
+                // 액티비티 이동시 페이드인/아웃 효과를 보여준다. 즉, 인트로
+                //    화면에 부드럽게 사라진다.
+                overridePendingTransition(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+            }
+        }, 0000);
+    }
+
+    // 인트로 화면 이후.
+    private void afterIntro(Bundle savedInstanceState) {
+        // 기본 테마를 지정한다.
+        setTheme(R.style.AppTheme);
+        setContentView(R.layout.activity_main);
+    }
 
 }
